1e5fd362ae285405fdcfc88555e35c67b65e3061
Libby Marchetti <lsmarchetti01@gmail.com>
2022-03-23 21:52:43 -0500

    added pseudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//create a multiple choice quiz
+    //each answer is a button
+        //eventListener("click,()")
+
+    //result will pop-up with correct/incorrect on timer
+        //if(answer correct){display correct for time} else {display wrong for time}
+        //if answer wrong timer penalty issued
+
+    //onclick next question and set of answers pops up
+
+//create timer for quiz
+    //done when timer is up
+    //done when quiz is finished
+
+//create highscores
+    //each correct answer has a point value
+    //point tracker
+    //user can input initals to store highscore
+
+//cry and call lyzzi
